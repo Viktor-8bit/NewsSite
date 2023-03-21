3f3cf556c7d46c39cb64a39abbed9e7a16f0d89d
Ivan K <64777603+Viktor-8bit@users.noreply.github.com>
2023-03-21 10:40:09 +0700

    ver 0.0.6
    Добавил {{ error }} в которую мы засовываем ошибки 🍩🍩🍩

--- Main/_NewsSite/views.py
+++ Main/_NewsSite/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-
-
 from .forms import *
-
 import hashlib
 
 #страницы храняться по адресу NewsSite\Main\site_pages
-
-    # тестовый аккаунт - login: AMOGUS pass:  9E6-rH9-Ad9-FE6
+# тестовый аккаунт - login: AMOGUS pass:  9E6-rH9-Ad9-FE6
 
 def registration_page(request):
     if request.method == 'GET':
@@ -23,7 +15,7 @@ def registration_page(request):
             try:
                 User_count = Users.objects.filter(Login =regform.cleaned_data['Login']).count()
                 if User_count > 0:
-                    return HttpResponse('пользователь с таким Login существует в базе, не взламывайте его перебором паролей пожалуйста 😏!!!')
+                    return render(request, 'reg.html', {'reg': RegForm(), 'error': 'пользователь с таким Login существует в базе, не взламывайте его перебором паролей пожалуйста 😏!!!'})
                 else:
                     user = Users()
                     user.email = regform.cleaned_data['email']
@@ -31,7 +23,7 @@ def registration_page(request):
                     user.password = hashlib.sha256(bytes(regform.cleaned_data['password'], 'utf-8')).hexdigest()
                     user.save()
             except:
-                return  HttpResponse('упс, где-то произошла ошибочка 😨')
+                return render(request, 'reg.html', { 'reg': RegForm(), 'error': 'упс, где-то произошла ошибочка 😨'} )
         return HttpResponse('Вы скорее всего успешно зарегистрированы 😎')
 
 def login_page(request):
@@ -48,16 +40,16 @@ def login_page(request):
 
                 if len(User_count) > 0:
                     print(User_count[0])
-                    return HttpResponse(f'аккаунт существует 🍻 с логином {login} и паролем {password}')
+                    return HttpResponse(f'аккаунт существует 🍵 с логином {login} и паролем {password} 🍵')
                 else:
-                    return  HttpResponse('такого пользователя нет ️‍🐒️ или пароль неправильный 🐒')
+                    return render(request, 'login.html', {'login': LoginFrom(), 'error': 'такого пользователя нет ️🐒️ или пароль неправильный 🐒️'})
             except Exception as ex:
                 template = "An exception of type {0} occurred. Arguments:\n{1!r}"
                 message = template.format(type(ex).__name__, ex.args)
                 print(message)
 
-                return HttpResponse('упс, где-то произошла ошибочка ☕')
-    return HttpResponse('неизвестная ошибка 😨')
+                return render(request, 'login.html', {'login': LoginFrom(), 'error': 'упс, где-то произошла ошибочка ☕'})
+    return HttpResponse('неизвестная ошибка 🕷🕸')
 
 def index(request):
     return HttpResponse('hello page')
